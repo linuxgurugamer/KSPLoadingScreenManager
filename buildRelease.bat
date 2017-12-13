b579da35f6eadca40db6af4fc8ad705f8e08c2c2
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-12-13 08:13:53 -0500

    Added Jenkins config

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,15 +1,50 @@
 
 @echo off
 
-copy /Y LoadingScreenManager\bin\Release\LoadingScreenManager.dll GameData
+rem Put the following text into the Post-build event command line:
+rem without the "rem":
 
-copy /Y ..\MiniAVC.dll GameData\LoadingScreenManager
+rem start /D D:\Users\jbb\github\IFI-Life-Support /WAIT deploy.bat  $(TargetDir) $(TargetFileName)
+rem 
+rem if $(ConfigurationName) == Release (
+rem 
+rem start /D D:\Users\jbb\github\IFI-Life-Support /WAIT buildRelease.bat $(TargetDir) $(TargetFileName)
+rem 
+rem )
+
+
+rem Set variables here
+
+rem H is the destination game folder
+rem GAMEDIR is the name of the mod folder (usually the mod name)
+rem GAMEDATA is the name of the local GameData
+rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
+rem    but not always
+rem LICENSE is the license file
+rem README is the readme file
+
+set GAMEDIR=LoadingScreenManager
+set GAMEDATA="GameData\"
+set VERSIONFILE=%GAMEDIR%.version
+set LICENSE=License.txt
+set README=README.md
 
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
 
-copy GameData\LoadingScreenManager\LoadingScreenManager.version a.version
-set VERSIONFILE=a.version
+rem Copy files to GameData locations
+
+copy /Y "%1%2" "%GAMEDATA%"
+copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
+copy /Y ..\MiniAVC.dll %GAMEDATA%\%GAMEDIR%
+
+if "%LICENSE%" NEQ "" copy /y  %LICENSE% %GAMEDATA%\%GAMEDIR%
+if "%README%" NEQ "" copy /Y %README% %GAMEDATA%\%GAMEDIR%
+
+rem Get Version info
+
+copy %VERSIONFILE% tmp.version
+set VERSIONFILE=tmp.version
 rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
 c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
 set /P major=<tmpfile
@@ -23,16 +58,18 @@ set /P patch=<tmpfile
 c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
 set /P build=<tmpfile
 del tmpfile
+del tmp.version
 set VERSION=%major%.%minor%.%patch%
 if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
 
-del a.version
 echo Version:  %VERSION%
 
-copy /y /s License.txt GameData\LoadingScreenManager
-copy /Y LoadingScreenManager\README.md GameData\LoadingScreenManager
 
+rem Build the zip FILE
+cd %GAMEDATA%\..
 
-set FILE="%RELEASEDIR%\LoadingScreenManager-%VERSION%.zip"
+set FILE="%RELEASEDIR%\%GAMEDIR%-%VERSION%.zip"
 IF EXIST %FILE% del /F %FILE%
 %ZIP% a -tzip %FILE% GameData
+
+pause
--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,21 @@
 
+
+@echo off
+
+rem H is the destination game folder
+rem GAMEDIR is the name of the mod folder (usually the mod name)
+rem GAMEDATA is the name of the local GameData
+rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
+rem    but not always
+
 set H=R:\KSP_1.3.1_dev
-echo %H%
+set GAMEDIR=LoadingScreenManager
+set GAMEDATA="GameData\"
+set VERSIONFILE=%GAMEDIR%.version
 
-copy /Y LoadingScreenManager\bin\Debug\LoadingScreenManager.dll GameData
+copy /Y "%1%2" "%GAMEDATA%"
+copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
 
-cd GameData
-
-copy LoadingScreenManager.dll %H%\GameData
-mkdir "%H%\GameData\LoadingScreenManager"
-xcopy /y /s LoadingScreenManager "%H%\GameData\LoadingScreenManager"
+pause
